2217f50c5aae71995553cbf4fc2593ce61c2b4d9
Steave Roger <steavenroger@gmail.com>
2021-06-02 02:30:43 +0700

    Bug Fix

--- Rest API/application/controllers/index.php
+++ Rest API/application/controllers/index.php
@@ -10,33 +10,35 @@
 
             if (!in_array(splice(1), $ignore))
             {
-
-                header_content_type("json");
-
-                $token = token();
-                $query = $this->db_select("data_token", ["token" => $token]);
-
-                if ($query->status == 1)
+                if (token() !== false)
                 {
-                    if (time() > $query->end_date)
-                    {
-                        $this->db_update("data_token", ["status" => 0, "where-token" => $token]);
+                    header_content_type("json");
 
+                    $token = token();
+                    $query = $this->db_select("data_token", ["token" => $token]);
+
+                    if ($query->status == 1)
+                    {
+                        if (time() > $query->end_date)
+                        {
+                            $this->db_update("data_token", ["status" => 0, "where-token" => $token]);
+
+                            $json["response"] = false;
+                            $json["message"] = "Token Expired.";
+
+                            echo json_encode($json);
+                            exit;
+                        }
+                    }
+
+                    if ($query->status == 0)
+                    {
                         $json["response"] = false;
                         $json["message"] = "Token Expired.";
 
                         echo json_encode($json);
                         exit;
                     }
-                }
-
-                if ($query->status == 0)
-                {
-                    $json["response"] = false;
-                    $json["message"] = "Token Expired.";
-
-                    echo json_encode($json);
-                    exit;
                 }
             }
         }
